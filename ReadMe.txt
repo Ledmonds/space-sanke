507148607e45b94067f8809b2c6d70d620a45a2f
Ledmonds <39761293+Ledmonds@users.noreply.github.com>
2018-06-24 11:06:11 +0200

    Added files via upload.
    General code Update
    
    Feature Additions:
    Spilt Bugs and Todo into seperate files.
    Play again option. //Click mouse when dead to start game again.
    Upload to Github and use to manage code.
    Add Sin/Fading text function.
    Implement delta time for movement, over using the frame rate as speed can not be as yet correctly calculated.
    Fix the Border drawing function, use Board class to push border CoOrds back to environment class [x1,y1,x2,y2,node_size];
    
    Bug Fixes:
    202:	New snake body tile is spawned at [0,0] and not snake tail.
    
    Known Bugs:
    101:	When screen is resized the Press any key to play button comes back.
    201:	Snakehead leaves screen by 1 iteration when dead.

--- ReadMe.txt
+++ ReadMe.txt
@@ -11,29 +11,3 @@ Board.js 			//Contains code relevant to the playing area for the Snake game, suc
 Environment.js 		//Contains code relevant to the general screen environment, such as background, information text, etc.
 FloatingText.js 	//Contains code relevant to creating text based effects used within game.
 Particles.js:		//Contains code relevant to creating custom particle effects in game, such as particles when the Snake eats some good.
-
-ToDo:
-==========
-Easy:
-=====
-Play again button.
-Eaten food colour block within snake; shows which posistion food was in when it was eaten by the snake.
-Draw a tounge that randomly comes out from the snake, maybe as it traverses towards the food.
-Upload to Github and use to manage code.
-Rethink naming convention for border as this is to similar to board.
-
-Medium:
-=====
-Food randomiseCoOrds function to check if it collides with part of the snake body to stop it from spawning where the snake already is.
-Some background effects outside of the border: maybe a few snakes randomly traversing the terrain in the background while the user plays.
-Fix the Border drawing function, use Board class to push border CoOrds back to environment class [x1,y1,x2,y2,node_size];
-
-Hard:
-=====
-Implement delta time for movement, over using the frame rate as speed can not be as yet correctly calculated.
-Port settings to external settings file.
-
-Bugs:
-=====
-Snakehead leaves screen by 1 iteration when dead.
-When screen is resized the Press any key to play button comes back.
--- main.js
+++ main.js
@@ -1,40 +1,69 @@
 /*==========
+Variables
+==========*/
+speed = 40;
+
+
+/*==========
 Setup
 ==========*/
-function setup() {	
-  	createCanvas(window.innerWidth, window.innerHeight);
-    frameRate(20);
-  	
-    //Declaration
-    environment = new Environment();
-    board = new Board([60,30],[window.innerWidth,window.innerHeight]);
+function setup() {
+		createCanvas(window.innerWidth, window.innerHeight);
+	
+		//Declaration
+		delta_time = new DeltaTime();
+		environment = new Environment();
+		board = new Board([60,30],[window.innerWidth,window.innerHeight]);
 
-    //Setup
-    environment.setupEnvironment(board.getBoardVector(),board.getNodeSize());
-  	board.getSnake().setupSnake();
+		//Setup
+		environment.setupEnvironment(board.getBoardSizeVector(),board.getNodeSize());
+		board.getSnake().setupSnake();
 }
 
 function windowResized() {
-  resizeCanvas(window.innerWidth,window.innerHeight);
-  board.boardResize([window.innerWidth,window.innerHeight]);
-  environment.setupEnvironment(board.getBoardVector(),board.getNodeSize()); 
+	resizeCanvas(window.innerWidth,window.innerHeight);
+	board.boardResize([window.innerWidth,window.innerHeight]);
+	environment.setupEnvironment(board.getBoardSizeVector(),board.getNodeSize()); 
+}
+
+function mouseClicked() {
+
+		if (!board.getSnake().getSnakeLiving()) {
+			//Declaration
+			environment = new Environment();
+			board = new Board([60,30],[window.innerWidth,window.innerHeight]);
+
+			//Setup
+			environment.setupEnvironment(board.getBoardSizeVector(),board.getNodeSize());
+			board.getSnake().setupSnake();
+		}
 }
 
 function keyPressed() {
-	if (keyCode === UP_ARROW && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(0);} 
-	else if (keyCode === RIGHT_ARROW && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(1);}
-	else if (keyCode === DOWN_ARROW && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(2);} 
-	else if (keyCode === LEFT_ARROW && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(3);}
+	if ((keyCode === UP_ARROW || keyCode === 87) && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(0);} 
+	else if ((keyCode === RIGHT_ARROW || keyCode === 68) && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(1);}
+	else if ((keyCode === DOWN_ARROW || keyCode === 83) && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(2);} 
+	else if ((keyCode === LEFT_ARROW || keyCode === 65) && !board.getSnake().getSnakeUpdatePhase()) {board.getSnake().setBearing(3);}
 	if (board.getSnake().getSnakeLooping()) {
-    board.getSnake().setSnakeLooping(false);
-    environment.disableStartText();
-  }
+		board.getSnake().setSnakeLooping(false);
+		environment.disableStartText();
+	}
 }
 
 /*==========
 Main
 ==========*/
 function draw() {
-	environment.setupEnvironmentScene(board.getScore());
-	board.iterateBoard();
+  //board.boardResize([window.innerWidth+(random(-50,20)),window.innerHeight+(random(-50,20))]);
+  //environment.setupEnvironment(board.getBoardSizeVector(),board.getNodeSize()); 
+
+
+	if (delta_time.getDeltaTimeReset() * board.getGameSpeed() >= 1) {
+		board.iterateBoard();
+		delta_time.setDeltaTimeReset();
+	}
+
+	environment.iterateEnvironment(board.getScore());
+	board.drawBoard();
+	delta_time.setDeltaTime();
 }
